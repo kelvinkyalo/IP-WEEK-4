bda46532f2e14fb0fe5587556182033c41259647
kelvinkyalo <kelvinchax@gmail.com>
2020-02-28 23:06:06 +0300

    style price list

--- js/script.js
+++ js/script.js
@@ -1,40 +1,40 @@
 $(document).ready(function() {
     $("button#one").click(function() {
-        $("ul#yoh").prepend("<li>Large(30cm)  __ __1150ksh</li>");
+        $("ul#yoh").prepend("<li>Large(30cm)  price =1150ksh + /- </li>");
 
     });
 
     $("button#two").click(function() {
-        $("ul#yohh").prepend("<li>Medium (23cm)  __ __800ksh</li>");
+        $("ul#yohh").prepend("<li>Medium (23cm)  price =800ksh + /- </li>");
 
     });
 
 
     $("button#three").click(function() {
-        $("ul#yohhh").prepend("<li>Small (19cm)  __ __600ksh</li>");
+        $("ul#yohhh").prepend("<li>Small (19cm)  price =600ksh + /- </li>");
     });
 
 
     $("button#cone").click(function() {
-        $("ul#yohhh").prepend("<li>Crispy ___150ksh</li>");
+        $("ul#yohhh").prepend("<li>Crispy  price =150ksh + /- </li>");
     });
     $("button#ctwo").click(function() {
-        $("ul#yohhh").prepend("<li>Stuffed  __ __150ksh</li>");
+        $("ul#yohhh").prepend("<li>Stuffed  price =150ksh + /- </li>");
     });
     $("button#cthree").click(function() {
-        $("ul#yohhh").prepend("<li>Gluten-free  __ __150ksh</li>");
+        $("ul#yohhh").prepend("<li>Gluten-free  price =150ksh + /- </li>");
     });
 
 
 
     $("button#tone").click(function() {
-        $("ul#yohhh").prepend("<li>Topping large  __ __250ksh</li>");
+        $("ul#yohhh").prepend("<li>Topping large   price =2150ksh + /- </li>");
     });
     $("button#ttwo").click(function() {
-        $("ul#yohhh").prepend("<li>Topping Medium  __ __150ksh</li>");
+        $("ul#yohhh").prepend("<li>Topping Medium   price =150ksh + /- </li>");
     });
     $("button#tthree").click(function() {
-        $("ul#yohhh").prepend("<li>Topping small  __ __100ksh</li>");
+        $("ul#yohhh").prepend("<li>Topping small  price =100ksh + /- </li>");
     });
 
 
