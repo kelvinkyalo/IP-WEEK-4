7b7208b59aa3d56fb91a990dcbdbee5d37706d0e
kelvinkyalo <kelvinchax@gmail.com>
2020-03-01 11:10:02 +0300

    get the total order

--- js/script.js
+++ js/script.js
@@ -48,3 +48,16 @@ $(document).ready(function() {
 
 
 });
+$(document).ready(function() {
+    var add = function(number1, number2) {
+        return number1 + number2;
+    };
+
+    $("form#add").submit(function(event) {
+        event.preventDefault();
+        var number1 = parseInt($("#add1").val());
+        var number2 = parseInt($("#add2").val());
+        var result = add(number1, number2);
+        $("#output").text(result);
+    });
+});
